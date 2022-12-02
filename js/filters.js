c8afb703c977f5d37e76b6a25db83809809dffc8
ArkoJano <arek0092@gmail.com>
2022-12-02 19:01:29 +0100

    fix: filters mechanism

--- js/filters.js
+++ js/filters.js
@@ -1,6 +1,6 @@
-function show_filters(){
+function show_filters() {
     let request = new XMLHttpRequest();
-    request.addEventListener("load", function(evt){
+    request.addEventListener("load", function (evt) {
         //console.log(evt);
     }, false);
 
@@ -9,11 +9,12 @@ function show_filters(){
 
     document.getElementById("filtr").innerHTML = request["responseText"];
 
+    document.getElementById('miasto').innerHTML = create_options(Object.keys(localizations), 'miasto')
 }
 
-function hide_filters(){
+function hide_filters() {
     let request = new XMLHttpRequest();
-    request.addEventListener("load", function(evt){
+    request.addEventListener("load", function (evt) {
         //console.log(evt);
     }, false);
 
@@ -22,4 +23,38 @@ function hide_filters(){
 
     document.getElementById("filtr").innerHTML = request["responseText"];
 
+}
+
+const localizations = {
+    'Kraków': ['DogNoHome - Kwiatowa 25, 30-437 Kraków', 'YetAnotherShelter - Opolska 28, 30-437 Kraków'],
+    'Wadowice': ['Schronisko Psia Łapa - Długa 75, 34-123 Wadowice'],
+    'Bydgoszcz': ['Get-A-Pet - Maślana 2, 72-122 Bydgoszcz'],
+    'Katowice': ['DoggyHome - Szaflarska 99, 40-000 Katowice']
+}
+
+function create_options(list, text){
+    let string = "";
+    let index = 0;
+    string += `<option value="" selected>Wybierz ${text}...</option>`
+    list.forEach(element => {
+        string += `<option value="${element}">${element}</option>`;
+
+    });
+    return string;
+}
+
+function switch_shelters(){
+
+    const city = document.getElementById('miasto').options
+    const city_selected_value = city[city.selectedIndex].value
+
+
+    if (city_selected_value == "" || Object.keys(localizations[city_selected_value]).length == 0){
+        document.getElementById('schronisko').setAttribute('disabled', '')
+        return
+    } else {
+        document.getElementById('schronisko').removeAttribute('disabled')
+    }
+
+    document.getElementById('schronisko').innerHTML = create_options(localizations[city_selected_value], 'konkretne schronisko')
 }
